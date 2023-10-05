7eeab9d1833b03f6b1cde63be5d30981acc97be4
TheShadow03 <adamoiuflorin999@gmail.com>
2023-10-05 12:31:04 +0300

    mesaj custom pins yada

---
+++ components/CustomPins/Custom_pins.tsx
@@ -0,0 +1,14 @@
+import L from "leaflet";
+
+const  LeafIcon = L.Icon.extend({
+    options: {
+        shadowUrl: 'leaf-shadow.png',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    }
+});
+
+export default LeafIcon;
